3d5a3be2d3c2fd71f458ad2cd7d4af07adf6fe6e
arcaneless <kings88819@outlook.com>
2019-06-28 20:16:14 +0800

    organized code

--- tictactoe.go
+++ tictactoe.go
@@ -3,13 +3,6 @@ package main
 import "fmt"
 const exs = -1000000
 
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
 func max(a int , b int) int {
 	if a > b {
 		return a
@@ -30,134 +23,83 @@ func min(a int, b int) int {
 
 
 type GameBoard struct {
-	board [3][3]int
+	board [9]int
 	state int
 }
 
 func (game *GameBoard) printBoard() {
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
-			fmt.Printf("%d ", game.board[i][j])
+	for i:=0; i<9; i++ {
+		fmt.Printf("%d ", game.board[i])
+		if (i+1)%3 == 0 {
+			fmt.Printf("\n")
 		}
-		fmt.Printf("\n")
 	}
 }
 
 func (game *GameBoard) tie() bool {
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
-			if game.board[i][j] == 0 {
-				return false
-			}
+	for i:=0; i<9; i++ {
+		if game.board[i] == 0 {
+			return false
 		}
 	}
 	return true
 }
 
-func (game *GameBoard) calScore() ([3]int, [3]int, int, int) {
-	var v, h [3]int
-	var c1, c2 int
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
-			h[i] += game.board[i][j]
-			v[i] += game.board[j][i]
-		}
+func (game *GameBoard) calScore() (int) {
+	wins := [8][3]int{{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}}
+	for i:=0; i<8; i++ {
+		if (game.board[wins[i][0]] != 0 &&
+		   (game.board[wins[i][0]] == game.board[wins[i][1]]) &&
+		   (game.board[wins[i][0]] == game.board[wins[i][2]]) {
+				return game.board[wins[i][0]]
+		   }
 	}
-	c1 = game.board[0][0] + game.board[1][1] + game.board[2][2]
-	c2 = game.board[0][2] + game.board[1][1] + game.board[2][0]
-	
-	return v, h, c1, c2
+	return 0
 }
 
 func (game *GameBoard) isGameDone() int {
-	v, h, c1, c2 := game.calScore()
-
-	var flagWin, flagLost int
-	flagWin = 10000
-	flagLost = 10000
-
-	for i:=0; i<3; i++ {
-		if (v[i] == -2) || (h[i] == -2) {
-			flagLost = -5
-		}
-		if (v[i] == 2) || (h[i] == 2) {
-			flagWin = 5
-		}
-	}
-	if (c1 == -2) || (c2 == -2) {
-		flagLost = -5
-	}
-	if (c1 == 2) || (c2 == 2) {
-		flagWin = 5
-	}
-	if flagWin < 100 || flagLost < 100 {
-		return min(flagWin, flagLost)
-	}
-
-	return 0
-
-}
-
-func (game *GameBoard) isGameFinished() int {
 	if game.tie() {
 		fmt.Println("tie")
-		return 0
+		return 10
 	}
-	v, h, c1, c2 := game.calScore()
 
-	
-	for i:=0; i<3; i++ {
-		if (v[i] == -3) || (h[i] == -3) {
-			return -1
-		}
-		if (v[i] == 3) || (h[i] == 3) {
-			return 1
-		}
-	}
-	if (c1 == -3) || (c2 == -3) {
-		return -1
-	}
-	if (c1 == 3) || (c2 == 3) {
-		return 1
+	if game.calScore() != 0 {
+		return game.calScore()
 	}
 
 	return 0
 }
 
 
-func minimax(game GameBoard, depth int) int {
+func minimax(game GameBoard) int {
 	var v int
-	copy := game
 	if game.tie() {
 		return 0
 	}
-	if game.isGameDone() != 0 && depth > 0 {
-		fmt.Println("Depth", depth)
-		return game.isGameDone()
+	if game.calScore() != 0 {
+		return game.calScore()
 	}
 	// simulate move
-				
 	// max
 	v = exs
 
 
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
-			if copy.board[i][j] == 0 {
-				copy.board[i][j] = copy.state
-				copy.state = -copy.state
+	for i:=0; i<9; i++ {
+		if game.board[i][j] == 0 {
+			// Init the trial
+			game.board[i][j] = copy.state
+			game.state = -copy.state
 
-				fmt.Println("dd", depth)
-				copy.printBoard()
-				v = max(v, minimax(copy, depth+1))
-				fmt.Println("v", v)
-				
-				copy.board[i][j] = 0
-				copy.state = -copy.state
-			}
+			copy.printBoard()
+			fmt.Println("try")
+			v = max(v, minimax(game))
+
+			// Reset the trial
+			game.board[i][j] = 0
+			game.state = -game.state
 		}
 	}
-	fmt.Println("minimax: ", v, " depth: ", depth)
+	fmt.Println("minimax: ", v)
 
 
 	return v
@@ -165,22 +107,23 @@ func minimax(game GameBoard, depth int) int {
 
 func findMoveviaScore(game GameBoard, score int) (int, int) {
 	v := exs
-	for i:=0; i<3; i++ {
-		for j:=0; j<3; j++ {
-			if game.board[i][j] == 0 {
-				game.board[i][j] = game.state
-				game.state = -game.state
+	for i:=0; i<9; i++ {
+		if game.board[i][j] == 0 {
+			// Init the trial
+			game.board[i][j] = copy.state
+			game.state = -copy.state
 
-				v = max(v, minimax(game, 1))
-				//fmt.Printf
+			copy.printBoard()
+			fmt.Println("try")
+			v = max(v, minimax(game))
 
-				game.board[i][j] = 0
-				game.state = -game.state
+			// Reset the trial
+			game.board[i][j] = 0
+			game.state = -game.state
 
-				if v == score {
-					fmt.Println("Best Score: ", v)
-					return i, j
-				}
+			if v == score {
+				fmt.Println("Best Score: ", v)
+				return i, j
 			}
 		}
 	}
